9eec1a0694a01ef9fb9f8033210d18dcf3c6be06
Viktor <36134532+YoungHamster@users.noreply.github.com>
2018-10-11 11:07:48 +0400

    Wrote 2 functions
    Wrote SendAllPhysics and Disconnect functions on informatics class at school

--- Server/NetworkEngine.cpp
+++ Server/NetworkEngine.cpp
@@ -68,142 +68,6 @@ void NetworkEngine::AsyncUserConnection(GameRoom *firstRoom, connection connecti
 	}
 }
 
-//void NetworkEngine::Handshake(connection client, GameRoom *firstRoom)
-//{
-//	GameRoom *room = firstRoom;
-//	int nicknamelenght = 0;
-//	char buff[1500];
-//	ZeroBuff(&buff[0], 1500);
-//
-//	recv(client.connectSock, buff, sizeof(buff), NULL);
-//	if (buff[0] != '\x01')
-//	{
-//		std::cout << "Error while handshaking with player" << std::endl;
-//		closesocket(client.connectSock);
-//		client.connected = false;
-//		return;
-//	}
-//	for (int i = 0; i < sizeof(buff) - 1; i++)
-//	{
-//		if (buff[i + 1] == '\0') continue;
-//		nicknamelenght++;
-//	}
-//	wchar_t* nickname = new wchar_t[nicknamelenght / 2];
-//	for (int i = 0; i < nicknamelenght / 2; i++)
-//	{
-//		/*nickname[i] = 0;
-//		nickname[i] |= buff[i + 1];
-//		nickname[i] <<= 8;
-//		nickname[i] |= buff[i + 2];*/
-//		WcharToBytes nicknameConv;
-//		nicknameConv.bytes[0] = buff[i + 1];
-//		nicknameConv.bytes[1] = buff[i + 2];
-//		nickname[i] = nicknameConv.wchar;
-//	}
-//
-//	/* Respone */
-//	int bufflenght = 9;
-//	client.nickname = nickname;
-//	client.uuid |= randNum();
-//	client.uuid <<= 32;
-//	client.uuid |= randNum();
-//	buff[0] = '\x01';
-//	ULLToBytes uuidConv;
-//	uuidConv.integer = client.uuid;
-//	for (int i = 0; i < 8; i++)
-//	{
-//		
-//		buff[i + 1] = uuidConv.bytes[i];
-//	}
-//	if (AnyActiveRooms(firstRoom))
-//	{
-//		buff[9] = '\x02';
-//		room = firstRoom;
-//		for (int i = 0; i < 250; i++, room++)
-//		{
-//			if (room->roomActive)
-//			{
-//				IntToBytes URIDconv;
-//				URIDconv.integer = room->URID;
-//				buff[10 + i * sizeof(int)] = URIDconv.bytes[0];
-//				buff[11 + i * sizeof(int)] = URIDconv.bytes[1];
-//				buff[12 + i * sizeof(int)] = URIDconv.bytes[2];
-//				buff[13 + i * sizeof(int)] = URIDconv.bytes[3];
-//				buff[14 + i * sizeof(int)] = '\0';
-//				bufflenght = 14 + i * sizeof(int);
-//			}
-//		}
-//	}
-//	else
-//	{
-//		buff[9] = '\0';
-//	}
-//	send(client.connectSock, buff, bufflenght, NULL);
-//	
-//	/* Get */
-//	ZeroBuff(&buff[0], 1500);
-//	recv(client.connectSock, buff, sizeof(buff), NULL);
-//	if (buff[0] != '\x02')
-//	{
-//		closesocket(client.connectSock);
-//		client.connected = false;
-//		return;
-//	}
-//	if (buff[1] == '\x01')
-//	{
-//		GameRoom newroom;
-//		newroom.Init(client);
-//		std::cout << "Recently connected client data: nickname - " << client.nickname << " uuid - " << client.uuid << " room id - " << newroom.URID << std::endl;
-//		room = NewRoom(newroom, firstRoom);
-//		buff[0] = '\x03';
-//		IntToBytes i2b;
-//		i2b.integer = room->URID;
-//		buff[1] = i2b.bytes[0];
-//		buff[2] = i2b.bytes[1];
-//		buff[3] = i2b.bytes[2];
-//		buff[4] = i2b.bytes[3];
-//		buff[5] = '\0';
-//		send(client.connectSock, buff, 6, NULL);
-//		std::thread NewRoom(AsyncRoomThr, room);
-//		NewRoom.detach();
-//		/* Instead of creating new thread i call this function in this thread */
-//		/*std::thread newPlayer(AsyncUserConnectionThr, room, &room->players[0]);
-//		newPlayer.detach();*/
-//		AsyncUserConnectionThr(room, &room->players[0]);
-//		return;
-//	}
-//	if (buff[1] == '\x02')
-//	{
-//		IntToBytes i2b;
-//		i2b.bytes[0] = buff[2];
-//		i2b.bytes[1] = buff[3];
-//		i2b.bytes[2] = buff[4];
-//		i2b.bytes[3] = buff[5];
-//		//BytesToInt(&buff[2], &urid);
-//		room = firstRoom;
-//		for (int i = 0; i < 1000; i++, room++)
-//		{
-//			if (room->URID == i2b.integer)
-//			{
-//				buff[0] = '\x03';
-//				buff[5] = '\0';
-//				int newclientid = room->ConnectPlayer(client);
-//				send(client.connectSock, buff, 6, NULL);
-//				std::cout << L"Recently connected client data: nickname - " << client.nickname << L" uuid - " << client.uuid << " room id - " << room->URID << std::endl;
-//
-//				/* Instead of creating new thread i call this function in this thread */
-//				/*std::thread newPlayer(AsyncUserConnectionThr, room, &room->players[newclientid]);
-//				newPlayer.detach();*/
-//				AsyncUserConnectionThr(room, &room->players[newclientid]);
-//				return;
-//			}
-//		}
-//	}
-//	std::cout << "Error while handshaking with player\n";
-//	client.connected = false;
-//	closesocket(client.connectSock);
-//}
-
 GameRoom* NetworkEngine::NewRoom(GameRoom newroom, GameRoom* firstRoom)
 {
 	for (int i = 0; i < 250; i++)
@@ -670,20 +534,131 @@ int Ping(char* buff, int buffSize, GameRoom *firstRoom, connection *player)
 
 int SendOptimizedPhysics(char* buff, int buffSize, GameRoom *firstRoom, connection *player)
 {
-
+	// 2 BYTES-HEADER, 4 BYTES-SNAKESDIRECTIONS, 4 BYTES-2 SHORTS(APPLE AABB.min), 8 BYTES - 4 SHORTS(LENGHTS OF SNAKES)
+	char oneTickPacket[2 + 4 + 4 + 8];
+	oneTickPacket[0] = 0x05;
+	oneTickPacket[1] = 0x01;
+	oneTickPacket[2] = GameRoom->players[0].GameDir;
+	oneTickPacket[3] = GameRoom->players[1].GameDir;
+	oneTickPacket[4] = GameRoom->players[2].GameDir;
+	oneTickPacket[5] = GameRoom->players[3].GameDir;
+	ShortToBytes converter;
+	for (int i = 0; i < room->GetPhysicsForPlayer().size(); i++)
+	{
+		if (room->GetPhysicsForPlayer()[i].type == APPLE)
+		{
+			converter.integer = GameRoom->GetPhysicsForPlayer()[i].borders.min.x;
+			oneTickPacket[6] = converter.bytes[0];
+			oneTickPacket[7] = converter.bytes[1];
+			converter.integer = GameRoom->GetPhysicsForPlayer()[i].borders.min.y;
+			oneTickPacket[8] = converter.bytes[0];
+			oneTickPacket[9] = converter.bytes[1];
+			i = room->GetPhysicsForPlayer().size();
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		converter.integer = GameRoom->GetSnakeLenght(i);
+		oneTickPacket[10 + i * 2] = converter.bytes[0];
+		oneTickPacket[11 + i * 2] = converter.bytes[1];
+	}
+	sendresult = send(client->connectSock, oneTickPacket, sizeof(oneTickPacket), NULL);
 }
 
 int SendAllPhysics(char* buff, int buffSize, GameRoom *firstRoom, connection *player)
 {
-
+	short snake1Size = room->GetSnakeLenght(0);
+	short snake2Size = room->GetSnakeLenght(1);
+	short snake3Size = room->GetSnakeLenght(2);
+	short snake4Size = room->GetSnakeLenght(3);
+	// 2 bytes - packet header
+	// 10 bytes - 5shorts: lenght of physics vector, lenghts of 4 SnakeBlocks vector
+	short buffsize = physicssize * sizeof(PhysicalObject) + (snake1Size + snake2Size + snake3Size + snake4Size) * sizeof(SnakeBlock) + 2 + 10;
+	ShortToBytes buffsizeConverter; // union
+	buffsizeConverter.integer = buffsize;
+	send(client->connectSock, buffsizeConverter.bytes, sizeof(ShortToBytes), NULL); // send size of buffer for экономия of memory on client side
+	char *buff = new char[buffsize];
+	buff[0] = 0x06;
+	buff[1] = 0x02;
+	ShortToBytes lenghtsConverter;
+	lenghtsConverter.integer = physicssize;
+	buff[2] = lenghtsConverter.bytes[0];
+	buff[3] = lenghtsConverter.bytes[1];
+	lenghtsConverter.integer = snake1Size;
+	buff[4] = lenghtsConverter.bytes[0];
+	buff[5] = lenghtsConverter.bytes[1];
+	lenghtsConverter.integer = snake2Size;
+	buff[6] = lenghtsConverter.bytes[0];
+	buff[7] = lenghtsConverter.bytes[1];
+	lenghtsConverter.integer = snake3Size;
+	buff[8] = lenghtsConverter.bytes[0];
+	buff[9] = lenghtsConverter.bytes[1];
+	lenghtsConverter.integer = snake4Size;
+	buff[10] = lenghtsConverter.bytes[0];
+	buff[11] = lenghtsConverter.bytes[1];
+	
+	for(int i = 0; i < 4; i++)
+	{
+		leghtsConverter.integer = room->GetSnakeLenght(i);
+		buff[2 + i * sizeof(short)] = lenghtsConverter.bytes[0];
+		buff[3 + i * sizeof(short)] = lenghtsConverter.bytes[1];
+	}
+	
+	PhysicalObjectToBytes physicalObjectsConv;
+	for (int i = 0; i < physicssize; i++)
+	{
+		physicalObjectsConv.obj = room->GetPhysicsForPlayer()[i];
+		for (int j = 0; j < sizeof(PhysicalObject); j++)
+		{
+			buff[i * sizeof(PhysicalObject) + j + 12] = physicalObjectsConv.bytes[j];
+		}
+	}
+	SnakeBlockToBytes snakeBlocksConv; // union
+	for (int i = 0; i < snake1Size; i++)
+	{
+		snakeBlocksConv = GameRoom->GetSnake(0)[i];
+		int bytenum = i * sizeof(SnakeBlock) + physicssize * sizeof(PhysicalObject) + 12;
+		for(int j = 0; j < sizeof(SnakeBlock); j++)
+		{
+			buff[j + bytenum] = snakeBlocksConv.bytes[j];
+		}
+	}
+	for (int i = 0; i < snake2Size; i++)
+	{
+		snakeBlocksConv = GameRoom->GetSnake(1)[i];
+		int bytenum = snake1Size * sizeof(SnakeBlock) + i * sizeof(SnakeBlock) + physicssize * sizeof(PhysicalObject) + 12;
+		for(int j = 0; j < sizeof(SnakeBlock); j++)
+		{
+			buff[j + bytenum] = snakeBlocksConv.bytes[j];
+		}
+	}
+	for (int i = 0; i < snake3Size; i++)
+	{
+		snakeBlocksConv = GameRoom->GetSnake(2)[i];
+		int bytenum = snake1Size * sizeof(SnakeBlock) + snake2Size * sizeof(SnakeBlock) + i * sizeof(SnakeBlock) + physicssize * sizeof(PhysicalObject) + 12;
+		for(int j = 0; j < sizeof(SnakeBlock); j++)
+		{
+			buff[j + bytenum] = snakeBlocksConv.bytes[j];
+		}
+	}for (int i = 0; i < snake4Size; i++)
+	{
+		snakeBlocksConv = GameRoom->GetSnake(3)[i];
+		int bytenum = snake1Size * sizeof(SnakeBlock) + snake2Size * sizeof(SnakeBlock) + snake3Size * sizeof(SnakeBlock) + i * sizeof(SnakeBlock) + physicssize * sizeof(PhysicalObject) + 12;
+		for(int j = 0; j < sizeof(SnakeBlock); j++)
+		{
+			buff[j + bytenum] = snakeBlocksConv.bytes[j];
+		}
+	}
+	sendresult = send(client->connectSock, buff, buffsize, NULL);
 }
 
 int StopMatch(char* buff, int buffSize, GameRoom *firstRoom, connection *player)
 {
-
+	
 }
 
 int Disconnect(char* buff, int buffSize, GameRoom *firstRoom, connection *player)
 {
-
+	player->connected = false;
+	closesocket(player->sock);
 }
